540f4b649a5c7789fa3782a0b4b58c59f283d109
YoYo1012 <78294953+YoYo1012@users.noreply.github.com>
2021-04-14 20:14:33 +0800

    Add files via upload

---
+++ Scripts/Hunter/UserCtrl.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserCtrl : MonoBehaviour
+{
+    [SerializeField] GameObject freeLookCamera;
+    Character character;
+    bool isDead = false;
+    Animator animator;
+
+    bool isJump;//是否跳躍
+    public bool isAim;//是否瞄準中
+    bool Aim;//瞄準
+    Rigidbody moveing;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        character = GetComponent<Character>();
+    }
+
+    private void Update() {
+        if (!isJump) isJump = Input.GetButtonDown("Jump");
+        if (Input.GetMouseButtonDown(0))//按左鍵瞄準
+            Aim = true;
+        if (Input.GetMouseButtonUp(0))//放開相反
+            Aim = false;
+    
+
+    }
+    bool isMove;
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        if (!isDead)
+        {
+            if (!Aim)//不瞄準時
+            {
+                Vector3 camForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1));
+                Vector3 camRight = Vector3.Scale(Camera.main.transform.right, new Vector3(1, 0, 1));
+                Vector3 move = camForward * Input.GetAxis("Vertical") + camRight * Input.GetAxis("Horizontal");//WASD+相機方向移動
+
+                Vector2 movement = Vector2.zero;//角色XY值=0
+                character.Move(move, isJump, isAim, movement);//輸入變數給Move
+                isJump = false;
+                isAim = false;
+            }
+            else if (Aim)//瞄準時
+            {
+                Vector2 movement = new Vector2(Input.GetAxis("Vertical"), Input.GetAxis("Horizontal"));
+                Vector3 move = Vector3.zero;
+                isAim = true;
+                isJump = false;
+                character.Move(move, isJump, isAim, movement);
+            }
+        }
+    }
+
+    void SetDead() {
+        isDead = true;
+        freeLookCamera.SetActive(false);
+    }
+}
---
+++ Scripts/Warrior/TPCharacter.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TPCharacter : MonoBehaviour
+{
+    [SerializeField] float jumpPower = 8;
+    [SerializeField] float gravityMultiplier = 2;
+    [SerializeField] float groundCheckDistance = 0.4f;
+
+    Rigidbody rigidbody;
+    Animator animator;
+
+    bool isOnGround;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+    }
+
+    public void Move(Vector3 move, bool isJump) {
+        float checkDistance = rigidbody.velocity.y < 0.01f ? groundCheckDistance : 0.01f;
+        Vector3 groundNormal = Vector3.up;
+        isOnGround = false;
+        if(Physics.Raycast(transform.position + Vector3.up * 0.15f, Vector3.down, out RaycastHit hit, checkDistance)) {
+            isOnGround = true;
+            groundNormal = hit.normal;
+        }
+
+        if (isOnGround) {
+            if (isJump) {
+                rigidbody.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+            }
+        }
+        else {
+            rigidbody.AddForce((Physics.gravity * gravityMultiplier) - Physics.gravity);
+        }
+
+        move = Vector3.ProjectOnPlane(move, groundNormal);
+        move = transform.InverseTransformDirection(move);
+        float turnSpeed = Mathf.Lerp(180, 360, move.z);
+        transform.Rotate(0, Mathf.Atan2(move.x, move.z) * turnSpeed * Time.deltaTime, 0);
+
+        animator.SetFloat("Speed", move.z, 0.1f, Time.deltaTime);
+        animator.SetBool("Ground", isOnGround);
+        animator.SetFloat("vSpeed", rigidbody.velocity.y);
+    }
+
+    private void OnAnimatorMove() {
+        if (isOnGround) {
+            Vector3 v = animator.deltaPosition / Time.deltaTime;
+            v.y = rigidbody.velocity.y;
+            rigidbody.velocity = v;
+        }
+    }
+}
